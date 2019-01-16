2e78e8fa89598e953258ff57c5a0faf597c898e2
Guillermo Zafra <me@gzafra.com>
2019-01-16 14:31:04 +0100

    Initial Commit

---
+++ MusicBrowserTests/MusicBrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  MusicBrowserTests.swift
+//  MusicBrowserTests
+//
+//  Created by Guillermo Zafra on 16/01/2019.
+//  Copyright © 2019 Guillermo Zafra. All rights reserved.
+//
+
+import XCTest
+@testable import MusicBrowser
+
+class MusicBrowserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
